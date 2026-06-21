2b8d73d7c598ec42b7c1ec70375e883d418a7a5b
Shadowghost <Ghost_of_Stone@web.de>
2026-06-21 19:32:46 +0200

    Ensure Langauge header matches selected UI language

--- src/utils/jellyfin-apiclient/compat.ts
+++ src/utils/jellyfin-apiclient/compat.ts
@@ -1,7 +1,22 @@
 import { Api, Jellyfin } from '@jellyfin/sdk';
+import globalAxios from 'axios';
 import { ApiClient } from 'jellyfin-apiclient';
 
+import { getCurrentLocale } from 'lib/globalize';
 import { safeDecodeURIComponent } from 'utils/url';
+
+/**
+ * A shared Axios instance for SDK Api instances that injects the in-app selected
+ * language as the Accept-Language header..
+ */
+const axiosInstance = globalAxios.create();
+axiosInstance.interceptors.request.use(config => {
+    const locale = getCurrentLocale();
+    if (locale) {
+        config.headers.set('Accept-Language', locale);
+    }
+    return config;
+});
 
 /**
  * Returns an SDK Api instance using the same parameters as the provided ApiClient.
@@ -22,6 +37,7 @@ export const toApi = (apiClient: ApiClient): Api => {
         }
     })).createApi(
         apiClient.serverAddress(),
-        apiClient.accessToken()
+        apiClient.accessToken(),
+        axiosInstance
     );
 };
